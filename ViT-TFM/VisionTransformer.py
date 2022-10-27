e1a94fda88dd53107096c52a2bda1f234ce457ea
alo996 <a.richard@campus.lmu.de>
2022-10-27 20:07:00 +0200

    Cont. ViT implementation

--- ViT-TFM/VisionTransformer.py
+++ ViT-TFM/VisionTransformer.py
@@ -1,6 +1,60 @@
 import torch
 import torch.nn as nn
 
+
+def drop_path(x, drop_p=0., training=False):
+    """
+    Stochastic depth: Drop paths per sample (when applied in main path of residual blocks.)
+
+    Parameters
+    __________
+    x : torch.Tensor
+        Shape `(n_samples, 2, dspl_size, dspl_size)`
+
+    drop_p : float
+        Probability to drop sample from batch, thereby mimicking stochastic depth.
+
+    training : Bool
+        If True then we are in training mode and enable drop_path.
+
+    Returns
+    _______
+    torch.Tensor
+        Shape `(n_samples, 2, dspl_size, dspl_size)`
+    """
+    if drop_p == 0. or not training:
+        return x
+    keep_prob = 1 - drop_p
+    shape = (x.shape[0],) + (1,) * (x.ndim - 1)
+    random_tensor = x.new_empty(shape).bernoulli_(keep_prob)
+    if keep_prob > 0.:
+        random_tensor.div_(keep_prob)
+
+    return x * random_tensor
+
+
+class DropPath(nn.Module):
+    """
+    Stochastic depth: Drop paths per sample (when applied in main path of residual blocks.)
+
+    Parameters
+    __________
+    drop_p : float
+        Probability to drop sample from batch, thereby mimicking stochastic depth.
+
+    Attributes
+    __________
+    drop_p : float
+        Probability to drop sample from batch, thereby mimicking stochastic depth.
+    """
+    def __init__(self, drop_p=0.):
+        super().__init__()
+        self.drop_p = drop_p
+
+    def forward(self, x):
+        return drop_path(x, self.drop_p, self.training)
+
+
 class PatchEmbed(nn.Module):
     """Split image into patches and then embed them.
 
@@ -23,7 +77,7 @@ class PatchEmbed(nn.Module):
     proj : nn.Conv2d
         Convolutional layer to perform both the splitting into patches and their embedding.
     """
-    def _init__(self, dspl_size=104, patch_size=8, embed_dim=32):
+    def _init__(self, dspl_size=104, patch_size=8, embed_dim=2*104):
         super().__init__()
         self.dspl_size = dspl_size
         self.patch_size = patch_size
@@ -42,13 +96,14 @@ class PatchEmbed(nn.Module):
         Returns
         _______
         torch.Tensor
-        Shape `(n_samples, n_patches, embed_dim)
+            Shape `(n_samples, n_patches, embed_dim)
         """
-        x = self.proj(x) # (n_samples, embed_dim, n_patches ** 0.5, n_patches ** 0.5)
-        x = x.flatten(2) # (n_samples, embed_dim, n_patches)
-        x = x.transpose(1, 2) # (n_samples, n_patches, embed_dim)
+        x = self.proj(x)  # (n_samples, embed_dim, n_patches ** 0.5, n_patches ** 0.5)
+        x = x.flatten(2)  # (n_samples, embed_dim, n_patches)
+        x = x.transpose(1, 2)  # (n_samples, n_patches, embed_dim)
 
         return x
+
 
 class Attention(nn.Module):
     """
@@ -114,4 +169,268 @@ class Attention(nn.Module):
         if dim != self.dim:
             raise ValueError
 
-        qkv = self.qkv(x)
+        qkv = self.qkv(x)  # (n_samples, n_patches, 3 * dim)
+        qkv = qkv.reshape(
+            n_samples, n_tokens, 2, self.n_heads, self.head_dim
+        )  # (n_samples, n_patches, 2, n_heads, head_dim)
+        qkv = qkv.permute(2, 0, 3, 1 ,4) # (2, n_samples, n_heads, n_patches, head_dim)
+        q, k, v = qkv[0], qkv[1], qkv[2]
+
+        k_t = k.transpose(-2, -1)
+        dotprod = q @ k_t * self.scale  # (n_samples, n_heads, n_patches, n_patches)
+        attn = dotprod.softmax(dim=-1) # (n_samples, n_heads, n_patches, n_patches)
+        attn = self.attn_drop(attn)
+
+        weighted_avg = attn @ v  # (n_samples, n_heads, n_patches, head_dim)
+        weighted_avg = weighted_avg.transpose(1, 2)  # (n_samples, n_patches, n_heads head_dim)
+        weighted_avg = weighted_avg.flatten(2)  # (n_samples, n_patches, dim)
+        x = self.proj(weighted_avg)  # (n_samples, n_patches, dim)
+        x = self.proj_drop(x)  # (n_samples, n_patches, dim)
+
+        return x, attn
+
+
+class MLP(nn.Module):
+    """
+    Multilayer perceptron.
+
+    Parameters
+    __________
+    in_features : int
+        Number of input features.
+
+    hidden_features : int
+        Number of nodes in the hidden layer.
+
+    out_features : int
+        Number of output features.
+
+    p : float
+        Dropout probability.
+
+    Attributes
+    __________
+    fc1 : nn.Linear
+        First linear layer.
+
+    act : nn.GELU
+        Gaussian Error Linear Unit activation.
+
+    fc2 : nn.Linear
+        Second linear layer.
+
+    drop : nn.Droput
+        Dropout layer.
+    """
+    def __init__(self, in_features, hidden_features, out_features, p=0.):
+        super().__init__()
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = nn.GELU()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(p)
+
+    def forward(self, x):
+        """
+        Run forward pass.
+
+        Parameters
+        __________
+        x : torch.Tensor
+            Shape `(n_samples, n_patches, in_features)`
+
+        Returns
+        _______
+        torch.Tensor
+            Shape `(n_samples, n_patches, out_features)`
+        """
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+
+        return x
+
+
+class Block(nn.Module):
+    """
+    Transformer block.
+
+    Parameters
+    __________
+    dim : int
+        Embedding dimension.
+
+    n_heads : int
+        Number of attention heads.
+
+    mlp_ratio : float
+        Determines the hidden dimension size of the MLP with respect to `dim`.
+
+    qkv_bias : bool
+        If True then we include bias to query, key and value projections.
+
+    p, attn_p, drop_path : float
+        Dropout probabilities.
+
+    Attributes
+    __________
+    norm1, norm2 : nn.LayerNorm
+        Layer normalization.
+
+    attn : Attention
+        Attention module.
+
+    mlp : MLP
+        MLP module.
+    """
+    def __init__(self, dim, n_heads, mlp_ratio=4.0, qkv_bias=True, p=0., attn_p=0., drop_path=0.):
+        super().__init__()
+        self.norm1 = nn.LayerNorm(dim, eps=1e-6)
+        self.attn = Attention(dim, n_heads=n_heads, qkv_bias=qkv_bias, attn_p=attn_p, proj_p=p)
+        self.norm2 = nn.LayerNorm(dim, eps=1e-6)
+        hidden_features = int(dim * mlp_ratio)
+        self.mlp = MLP(in_features=dim, hidden_features=hidden_features, out_features=dim)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+    def forward(self, x, return_attention=False):
+        """
+        Run forward pass.
+
+        Parameters
+        __________
+        x : torch.Tensor
+            Shape `(n_samples, n_patches, dim)`
+
+        return_attention : Bool
+            Whether to also return attention or not.
+
+        Returns
+        _______
+        torch.Tensor
+            Shape `(n_samples, n_patches, dim)`
+
+        """
+        y, attn = self.attn(self.norm1)
+        x = x + self.drop_path(y)
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+
+        if return_attention:
+            return x, attn
+        else:
+            return x
+
+
+class VisionTransformer(nn.Module):
+    """
+    Implementation of Vision Transformer.
+
+    Parameters
+    __________
+    dspl_size : int
+        Height and width of sqaure displacement field.
+
+    patch_size : int
+        Height and width of patches.
+
+    embed_dim : int
+        Dimensionality of the token embeddings.
+
+    depth : int
+        Number of blocks.
+
+    n_heads : int
+        Number of attention heads.
+
+    mlp_ratio : float
+        Determines the hidden dimension of the `MLP` module.
+
+    p, attn_p. drop_path : float
+        Dropout probabilities.
+
+    Attributes
+    __________
+    patch_embed : PatchEmbed
+        Instance of PatchEmbed.
+
+    pos_embed : nn.Parameter
+        Positional embedding of all patches. It contains `n_patches * embed_dim` elements.
+
+    pos_drop : nn.Dropout
+        Dropout layer.
+
+    blocks : nn.ModuleList
+        List of `Block` modules.
+
+    norm : nn.LayerNorm
+        Layer normalization.
+    """
+    def __init__(
+            self,
+            dspl_size=104,
+            patch_size=8,
+            embed_dim=2*104,
+            depth=12,
+            n_heads=12,
+            mlp_ratio=4.,
+            qkv_bias=True,
+            p=0.,
+            attn_p=0,
+            drop_path=0.1
+    ):
+        super().__init__()
+        self.patch_embed = PatchEmbed(dspl_size, patch_size, embed_dim)
+        self.pos_embed = nn.Parameter(torch.zeros(1, self.patch_embed.n_patches, embed_dim))
+        self.pos_drop = nn.Dropout(p)
+        dpr = [x.item() for x in torch.linspace(0, drop_path, depth)]  # stochastic depth decay rule
+        self.blocks = nn.ModuleList(
+            [
+                Block(
+                    dim=embed_dim,
+                    n_heads=n_heads,
+                    mlp_ratio=mlp_ratio,
+                    qkv_bias=qkv_bias,
+                    p=p,
+                    attn_p=attn_p,
+                    drop_path=dpr[i]
+                )
+                for i in range(depth)
+            ]
+        )
+        self.norm = nn.LayerNorm(embed_dim, eps=1e-6)
+
+    def forward(self, x):
+        """
+        Run forward pass.
+
+        Parameters
+        __________
+        x : torch.Tensor
+            Shape `(n_samples, 2, dspl_size, dspl_size)`
+
+        Returns
+        _______
+        logits : torch.Tensor
+            Logits of reconstructed traction field, shape `(n_samples, 2, dspl_size, dspl_size)`
+        """
+        n_samples = x.shape[0]
+        x = self.patch_embed(x)
+        x = x + self.pos_embed  # (n_samples, n_patches, embed_dim)
+        x = self.pos_drop(x)
+
+        for block in self.blocks:
+            x = block(x)
+
+        x = self.norm(x)
+
+        return x
+
+    def _init_weight(self, m):
+        if isinstance(m, nn.Linear):
+            torch.nn.init.xavier_normal_(m.weight)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.LayerNorm):
+                nn.init.constant_(m.bias, 0)
+                nn.init.constant_(m.weight, 1.0)
+
