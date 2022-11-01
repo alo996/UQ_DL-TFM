6acb4d777f98de508f0678caaa8975db823eaf2d
alo996 <a.richard@campus.lmu.de>
2022-11-01 15:41:51 +0100

    Extenden main

--- ViT-TFM/main_dist.py
+++ ViT-TFM/main_dist.py
@@ -1,69 +1,62 @@
-import VisionTransformer as vit
-
 import argparse
 import copy
 import datetime
+import os
+from datetime import datetime
+from time import sleep
+
 import h5py
 import numpy as np
 import torch
+import torch.nn as nn
 import torch.backends.cudnn as cudnn
-
-from datetime import datetime
-from time import sleep
 from torch.utils.data import TensorDataset, DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
+
+import VisionTransformer as vit
 
 
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='TFM-ViT')
-    parser.add_argument('--batch-size', type=int, default=16, metavar='N',
+    parser.add_argument('--batch_size', type=int, default=16,
                         help='input batch size for training (default: 16)')
-    parser.add_argument('--val-batch-size', type=int, default=500, metavar='N',
+    parser.add_argument('--val_batch_size', type=int, default=500,
                         help='input batch size for validation (default: 500)')
-    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+    parser.add_argument('--epochs', type=int, default=10,
                         help='number of epochs to train (default: 100)')
     parser.add_argument('--patience', type=int, default=5,
                         help='Early stopping.')
-    parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
-                        help='initial learning rate (default: 0.001)')
-    parser.add_argument('--weight_decay', type=float, default=0.0005, metavar='WD',
-                        help='initial learning rate (default: 0.0005)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
+    parser.add_argument('--no_cuda', action='store_true', default=False,
                         help='disables CUDA training')
-    parser.add_argument('--dry-run', action='store_true', default=False,
+    parser.add_argument('--dry_run', action='store_true', default=False,
                         help='quickly check a single pass')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
+    # Optimizer settings
+    parser.add_argument('--lr', type=float, default=0.001,
+                        help='initial learning rate (default: 0.001)')
+    parser.add_argument('--weight_decay', type=float, default=0.0005,
+                        help='initial learning rate (default: 0.0005)')
+    parser.add_argument('--use_amp', type=float, default=True,
+                        help='use automatic mixed precision')
+    # Misc
+    parser.add_argument('--seed', type=int, default=1,
                         help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                        help='how many batches to wait before logging training status')
-    parser.add_argument('--save-model', action='store_true', default=False,
+    parser.add_argument('--num_workers', type=int, default=4,
+                        help='Number of data loading workers per GPU (default: 4')
+    parser.add_argument('--save_model', action='store_true', default=False,
                         help='For Saving the current Model')
     args = parser.parse_args()
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
 
+    # Setup distributed training and seeds
+    init_distributed_model(args)
     torch.manual_seed(args.seed)
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed(args.seed)
-    cudnn.benchmark = False
+    cudnn.benchmark = True
 
-    if use_cuda:
-        device = torch.device("cuda")
-    else:
-        device = torch.device("cpu")
-
-    train_kwargs = {'batch_size': args.batch_size}
-    val_kwargs = {'batch_size': args.val_batch_size}
-    if use_cuda:
-        num_workers = 4 * torch.cuda.device_count()
-        cuda_kwargs = {'num_workers': num_workers,
-                       'pin_memory': True,
-                       'shuffle': True}
-        train_kwargs.update(cuda_kwargs)
-        val_kwargs.update(cuda_kwargs)
-
+    # Preparing dataset
     dspl_train = h5py.File('data/displacements_25.h5')["data"]
     trac_train = h5py.File('data/tractions_25.h5')["data"]
     dspl_val = h5py.File('data/displacements_5.h5')["data"]
@@ -81,11 +74,14 @@ def main():
 
     train_set = TensorDataset(X_train, Y_train)
     val_set = TensorDataset(X_val, Y_val)
+    sampler = torch.utils.data.DistributedSampler(train_set, shuffle=True)
 
-    dataloaders = {}
-    dataloaders['train'] = DataLoader(train_set, **train_kwargs)
-    dataloaders['val'] = DataLoader(val_set, **val_kwargs)
+    train_kwargs = {'batch_size': args.batch_size, 'num_workers': args.num_workers, 'pin_memory': True}
+    val_kwargs = {'batch_size': args.val_batch_size, 'num_workers': args.num_workers, 'pin_memory': True}
+    dataloaders = {'train': DataLoader(train_set, sampler=sampler, **train_kwargs),
+                   'val': DataLoader(val_set, **val_kwargs)}
 
+    # Create model
     vit_model = vit.VisionTransformer(dspl_size=104,
                                       patch_size=8,
                                       embed_dim=128,
@@ -95,9 +91,18 @@ def main():
                                       p=0.1,
                                       attn_p=0.1,
                                       drop_path=0.1).double()
+    n_params = sum(p.numel() for p in vit_model.parameters() if p.requires_grad)
+    vit_model = nn.parallel.DistributedDataParallel(vit_model.cuda(), device_ids=[args.gpu])
 
+    # Loss and optimizer
     loss = torch.nn.MSELoss(reduction='mean')
     optimizer = torch.optim.AdamW(vit_model.parameters(), args.lr, args.weight_decay)
+    # amp_scaler = torch.cuda.amp.GradScaler() if args.use_amp else None
+
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
 
     NAME = "ViT-{:%Y-%b-%d %H:%M:%S}".format(datetime.now())
     writer = SummaryWriter(log_dir='{}'.format(NAME))
@@ -173,6 +178,43 @@ def run_epoch(model, loss_fn, dataloader, device, epoch, optimizer, train):
         tepoch.set_postfix(loss=epoch_loss)
         sleep(0.01)
     return epoch_loss, epoch_rmse
+
+
+def init_distributed_model(args):
+    if 'RANK' in os.environ and 'WORLD_SIZE' in os.environ:
+        args.rank = int(os.environ['RANK'])
+        args.world_size = int(os.environ['WORLD_SIZE'])
+        args.gpu = int(os.environ['LOCAL_RANK'])
+    elif 'SLURM_PROCID' in os.environ:
+        args.rank = int(os.environ['SLURM_PROCID'])
+        args.gpu = args.rank % torch.cuda.device_count()
+    else:
+        print('Not using distributed mode.')
+        args.distributed = False
+        return
+
+    args.distributed = True
+
+    torch.cuda.set_device(args.gpu)
+    args.dist_backend = 'nccl'
+    print('| distributed init (rank {}): {}'.format(args.rank, args.dist_url), flush=True)
+    torch.distributed.init_process_group(backend=args.dist_backend,
+                                         init_method=args.dist_url,
+                                         world_size=args.world_size,
+                                         rank=args.rank
+                                         )
+    torch.distributed.barrier()
+    setup_for_distributed(args.rank == 0)
+
+
+def setup_for_distributed(is_master):
+    import builtins as __builtin__
+    builtin_print = __builtin__.print
+
+    def print(*args, **kwargs):
+        force = kwargs.pop('force', False)
+        if is_master or force:
+            builtin_print(*args, **kwargs)
 
 
 if __name__ == '__main__':
